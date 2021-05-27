bdcd6c4a01f4dbcfa2f210ace9bac7d7c488fcd7
Antoine <antoine.lebras@gmail.com>
2021-05-27 15:17:27 +0200

    ébauche portfolio

---
+++ portfolio/app.js
@@ -0,0 +1,13 @@
+//Const
+var sec = document.getElementById('sec');
+var nav = document.getElementById('nav');
+
+//Listeneners
+
+//Functions
+
+//function to toggle the menu button -active class-
+function toggle(){
+    sec.classList.toggle('active');
+    nav.classList.toggle('active');
+}
