4826c15eea59208992562c4c7c87788d202f4ce9
Antoine <antoine.lebras@gmail.com>
2021-05-27 17:30:22 +0200

    ripple eff, neon logo, liens

--- portfolio/app.js
+++ portfolio/app.js
@@ -1,8 +1,25 @@
 //Const
 var sec = document.getElementById('sec');
 var nav = document.getElementById('nav');
+const sclButton = document.querySelectorAll('a')
 
 //Listeneners
+sclButton.forEach(btn => {
+    btn.addEventListener('click', function(e){
+
+        let x = e.clientX - e.target.offsetLeft;
+        let y = e.clientY - e.target.offsetTop;
+
+        let ripples = document.createElement('span');
+        ripples.style.left = x + 'px';
+        ripples.style.top = y + 'px';
+        this.appendChild(ripples);
+
+        setTimeout(() => {
+            ripples.remove()
+        },1000);
+    })
+})
 
 //Functions
 
